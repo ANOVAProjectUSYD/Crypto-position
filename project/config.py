67c6e94b3dab0ef419efb2d01482c19698e9eba8
Chris Hyland <christopherhyland95@gmail.com>
2018-02-13 22:56:11 +1100

    Removed stuff

--- project/config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://localhost/anova-crypto'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
